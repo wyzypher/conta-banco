2c453fdca07ed6d3b36f92727876265b248ef768
wyzypher <144710049+wyzypher@users.noreply.github.com>
2024-08-12 19:24:31 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
-
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
-        Scanner ler = new Scanner (System.in);
-
-        System.out.println("Informe o número da conta.");
-        int numero = ler.nextInt();
         
-        System.out.println("Informe o número da Agência com dígito.");
-        String agencia = ler.nextLine();
-
+        Scanner ler = new Scanner(System.in);
+        
         System.out.println("Informe o nome completo do titular.");
         String nomeCliente = ler.nextLine();
-
+        
+        System.out.println("Informe o número da conta.");
+        int conta = ler.nextInt();
+        
+        System.out.println("Informe o número da Agência com dígito.");
+        String agencia = ler.next();
+        
         System.out.println("Informe o valor do depósito.");
         float saldo = ler.nextFloat();
-
-        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo R$"+saldo+" já está disponível para saque");
-
-
+        
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo R$"+saldo+" já está disponível para saque");
+        
     }
 }
