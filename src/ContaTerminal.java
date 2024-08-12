0010f3841c8341f585d6aae7b30dbd0021330f96
JarbasJr <jarbasbenedito@gmail.com>
2024-08-12 14:34:47 -0300

    introdução de codigo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner ler = new Scanner (System.in);
+
+        System.out.println("Informe o número da conta.");
+        int numero = ler.nextInt();
+        
+        System.out.println("Informe o número da Agência com dígito.");
+        String agencia = ler.nextLine();
+
+        System.out.println("Informe o nome completo do titular.");
+        String nomeCliente = ler.nextLine();
+
+        System.out.println("Informe o valor do depósito.");
+        float saldo = ler.nextFloat();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo R$"+saldo+" já está disponível para saque");
+
+
     }
 }
